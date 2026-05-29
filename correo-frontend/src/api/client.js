96848332185dccd4ac6c70f4711a07f8809ff788
CamilaGutierrezCasas-ing <camiguticasas@outlook.com>
2026-05-29 18:19:22 -0500

    Corregir token en cliente axios

--- correo-frontend/src/api/client.js
+++ correo-frontend/src/api/client.js
@@ -9,7 +9,9 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   
-    const token = localStorage.getItem('token');
+    const token = storage.getToken();
+
+
     if (token) {
      config.headers.Authorization = `Bearer ${token}`;
   }
